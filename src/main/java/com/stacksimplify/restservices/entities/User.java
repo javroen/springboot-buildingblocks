0529724fe460b932424cf31199db3ba66c8b96e2
Joe Kassels <jkassels@atb.com>
2020-09-01 20:38:48 -0700

    JSonView internal/external

--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -13,35 +13,45 @@ import javax.validation.constraints.Size;
 
 import org.springframework.hateoas.RepresentationModel;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
 @Entity
 @Table(name = "user")
 public class User extends RepresentationModel<User>{
 	
 	@Id
 	@GeneratedValue
+	@JsonView(Views.External.class)
 	private long id;
 	
 	@NotEmpty(message="Username is mandatory")
 	@Column(name ="USER_NAME", length=50, nullable=false, unique=true)
+	@JsonView(Views.External.class)
 	private String username;
 	
 	@Size(min=2, message="Firstname should have at least 2 characters")
 	@Column(name ="FIRST_NAME", length=50, nullable=false)
+	@JsonView(Views.External.class)
 	private String firstname;
 	
 	@Column(name ="LAST_NAME", length=50, nullable=false)
+	@JsonView(Views.External.class)
 	private String lastname;
 	
 	@Column(name ="EMAIL_ADDRESS", length=50, nullable=false)
+	@JsonView(Views.External.class)
 	private String email;
 	
 	@Column(name ="ROLE", length=50, nullable=false)
+	@JsonView(Views.Internal.class)
 	private String role;
 	
 	@Column(name ="SSN", length=50, nullable=false, unique=true)
+	@JsonView(Views.Internal.class)
 	private String ssn;
 
 	@OneToMany(mappedBy="user")	
+	@JsonView(Views.Internal.class)
 	private List<Order> orders;
 	
 	public User() {
---
+++ src/main/java/com/stacksimplify/restservices/entities/Views.java
@@ -0,0 +1,12 @@
+package com.stacksimplify.restservices.entities;
+
+public class Views {
+	
+	//external
+	public static class External{
+	}
+	
+	//internal
+	public static class Internal extends External{
+	}
+}
